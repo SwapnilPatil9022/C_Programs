0d6c0160e7a3896b37370be9e847c1ebd2507b11
SwapnilPatil9022 <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-05-17 00:53:31 +0530

    My First C Program

---
+++ Add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int Addition(int , int);
+
+int main()
+{
+ int A = 10;
+ int B = 11;
+ int Ret = 0;
+
+ Ret =Addition(A , B);
+
+ printf("%",Ret);
+ return 0;
+}
+
+int Addition(int No1 , int  No2)
