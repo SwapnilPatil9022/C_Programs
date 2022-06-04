0effa6acd886605c2e6fe8e474a2878b7ca1b814
SwapnilPatil9022 <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-06-05 01:55:03 +0530

    Add files via upload

---
+++ pre.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define MAX 100
+
+int main()
+{
+ int no = 10;
+ no = no + MAX;
+ 
+ printf("Value ofs no is :%d\n",no);
+ 
+ return 0;
+ 
+}
+
+ 
+ 
+ 
---
+++ preprocessor.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define MAX 100
+
+int main()
+{
+ int no = 10;
+ no = no + MAX;
+ 
+ printf("Value of no is :%d\n",no);
+ 
+ return 0;
+ 
+}
+
+ 
+ 
+ 
+ 
+ 
+ 
